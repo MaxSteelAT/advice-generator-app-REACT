0f14a5dfb4434a1f177b4e6c25bf0b354751d7fe
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-02-10 15:27:38 -0800

    Se acthualizó el archivo de app.

--- src/App.js
+++ src/App.js
@@ -6,40 +6,8 @@ import { useState, useEffect } from 'react'
 
 
 function App() {
-  const appName = 'Application React JS'
-  const numbersArray = [2, 8, 7, 33, 5, 6, 88]
-  const [cat, setCat] = useState('Cirus')
-  const [inputDog, setInputDog] = useState('Brownies')
-
-  const exampleClick = () => {
-    console.log('Click')
-    setCat('Avena')
-
-  }
-
-  const changeDog = (event) => {
-    const value = event.target.value
-    setInputDog(value)
-  }
-
-  useEffect(() => {
-    console.log('Se esta cambiando', inputDog)
-  }, [inputDog])
-
   return (
     <div className="App">
-      <div className="App-header">
-        <h1>{appName}</h1>
-        <input onChange={changeDog} type='text' value={inputDog} ></input>
-        <h2>{inputDog}</h2>
-        <h2>{cat}</h2>
-        <RecipeTitle isClassTitle={true} rating={4} numbers={numbersArray} title='ingrid' />
-        <RecipeTitle isClassTitle={false} rating={3} numbers={numbersArray} title='israel' />
-        <div>{moment().format('DD/MM/YYYY')}</div>
-        <button onClick={exampleClick} style={{ marginTop: '100px' }}>Click</button>
-      </div>
-      <h1>Texto</h1>
-      <div>{Date.now()}</div>
     </div>
   );
 }
